aa3322d3f53793a4282a8d6f85d54f6a6df0206b
skyaki23 <bwctypmjhksk@gmail.com>
2020-04-17 12:59:57 +0800

    購物車顯示數量

---
+++ PhoneShop/PhoneShop/Content/main.js
@@ -0,0 +1,16 @@
+﻿
+function updateCartProducts() {
+    var cartProducts;
+    var existingCookieData = $.cookie('CartProducts');
+
+    if (existingCookieData != undefined && existingCookieData != "" && existingCookieData != null) {
+        cartProducts = existingCookieData.split('-');
+    }
+    else {
+        cartProducts = [];
+    }
+
+    document.getElementById('cartProductsCount').innerHTML = "<a href='/Home/ShoppingCart'>購物車(" + cartProducts.length + ")</a>";
+};
+
+updateCartProducts();
